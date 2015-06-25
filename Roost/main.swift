0ceb57a62ebc71dd6071720e426b4da5e8b170ec
Dirk Gadsden <dirk@esherido.com>
2015-06-25 14:16:34 -0700

    Update .gitignore; refactor main runner to be a function

--- Roost/main.swift
+++ Roost/main.swift
@@ -1,29 +1,33 @@
 import Foundation
 
-let fileManager = NSFileManager()
+func main() {
+  let fileManager = NSFileManager()
 
-let cwd = currentDirectoryPath()
-let path = NSString.pathWithComponents([cwd, "Roostfile"])
+  let cwd = currentDirectoryPath()
+  let path = NSString.pathWithComponents([cwd, "Roostfile"])
 
-if !fileManager.fileExistsAtPath(path) {
-  printAndExit("Missing Roostfile in '\(cwd)'")
+  if !fileManager.fileExistsAtPath(path) {
+    printAndExit("Missing Roostfile in '\(cwd)'")
+  }
+
+  let pathURL = NSURL(fileURLWithPath: path)!
+
+  var error: NSError?
+  let contents = NSString(contentsOfURL: pathURL, encoding: NSUTF8StringEncoding, error: &error)
+
+  if contents == nil {
+    println(error); exit(1)
+  }
+
+  let roostfile = Roostfile()
+  roostfile.directory = cwd
+  roostfile.parseFromString(contents! as String)
+
+  // roostfile.inspect()
+
+  let package = Package(roostfile)
+
+  package.compile()
 }
 
-let pathURL = NSURL(fileURLWithPath: path)!
-
-var error: NSError?
-let contents = NSString(contentsOfURL: pathURL, encoding: NSUTF8StringEncoding, error: &error)
-
-if contents == nil {
-  println(error); exit(1)
-}
-
-let roostfile = Roostfile()
-roostfile.directory = cwd
-roostfile.parseFromString(contents! as String)
-
-// roostfile.inspect()
-
-let package = Package(roostfile)
-
-package.compile()
+main()
