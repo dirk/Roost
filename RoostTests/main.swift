69fa22edde410bdff45fb61b9c83e2cd8a5a6578
Dirk Gadsden <dirk@esherido.com>
2015-08-01 13:05:17 -0700

    Implement basic Nimble assertion handler

--- RoostTests/main.swift
+++ RoostTests/main.swift
@@ -1,3 +1,14 @@
 import Nimble
 
+class NimbleAssertionHandlerAdapter: AssertionHandler {
+  func assert(assertion: Bool, message: FailureMessage, location: SourceLocation) {
+    if assertion { return }
+
+    println(message.stringValue)
+    println("  \(location.description)\n")
+  }
+}
+
+NimbleAssertionHandler = NimbleAssertionHandlerAdapter()
+
 expect(1 + 1).to(equal(3))
