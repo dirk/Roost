f3f7efff657a655067a1ab3e4126de45e7d41554
Dirk Gadsden <dirk@esherido.com>
2015-08-29 21:35:56 -0700

    Add additional Roostfile parsing test

--- RoostTests/RoostfileSpec.swift
+++ RoostTests/RoostfileSpec.swift
@@ -8,18 +8,31 @@ class RoostfileSpec: Spec {
         "name: Test",
         "version: 0.1.2",
         "target_type: executable",
+        "sources:",
+        "  - TestFile.swift",
+        "  - TestDirectory/",
       ])
     }
   }
 
   func spec() {
     describe("when parsing") {
-      it("should parse basic properties") {
-        let r = Roostfile()
-        r.parseFromString(self.definition)
+      var r: Roostfile!
 
+      beforeEach {
+        r = Roostfile()
+        r.parseFromString(self.definition)
+      }
+
+      it("should parse basic properties") {
         expect(r.name).to(equal("Test"))
         expect(r.targetType).to(equal(TargetType.Executable))
+      }
+
+      it("should parse sources") {
+        expect(r.sources.count).to(equal(2))
+        expect(r.sources[0]).to(equal("TestFile.swift"))
+        expect(r.sources[1]).to(equal("TestDirectory/"))
       }
     }
   }
